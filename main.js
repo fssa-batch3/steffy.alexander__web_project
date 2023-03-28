739cbc7f01fdff77b2d1dbce4b1fbae334afb1e2
steffydominic <steffybeena7@gmail.com>
2023-03-28 14:31:20 +0530

    fix: addded main.js

---
+++ main.js
@@ -0,0 +1,112 @@
+if (localStorage.getItem("create_plant")) {
+//   return;
+} 
+else {
+  localStorage.setItem(
+    "create_plant",
+    JSON.stringify([
+      {
+        image: "https://iili.io/HNNpjrQ.jpg",
+        price: "186",
+        name: "Hibiscus_plant",
+        ratings: "5",
+        image_1: "https://iili.io/HNNmEiv.jpg",
+        image_2: "https://iili.io/HNNmVlp.jpg",
+        image_3: "https://iili.io/HNNmhJI.jpg",
+        about: "One Healthy Live Plant In Pot\nLow maintenance home plant\n",
+        Plant_Name: "Hibiscus_plant",
+        Type_of_plant: "Flower",
+        Suitable_Location: "Indoor",
+        Container_Type: "Pot",
+        Height: "150 mm",
+        Brand: "Starlink",
+        Planting_Season: "Autumn",
+        table_Plantname: "Hibiscus_plant",
+        Hybrid: "No",
+        status: true,
+      },
+      {
+        image: "https://iili.io/HwCIiBa.jpg",
+        price: "175",
+        name: "Middlemist_Red",
+        ratings: "5",
+        image_1: "https://iili.io/HwazC3Q.jpg",
+        image_2: "https://iili.io/HwaISJp.jpg",
+        image_3: "https://iili.io/HwauwKJ.jpg",
+        about:
+          "\n You can only see middlemist red flower in New Zealand garden and green house garden of United Kingdom. ",
+        Plant_Name: "Middlemist_Red",
+        Type_of_plant: "Flower",
+        Suitable_Location: " Outdoor",
+        Container_Type: "Pot",
+        Height: " 6 to 12 feet.",
+        Brand: " E Garden",
+        Planting_Season: "Autumn",
+        table_Plantname: "Middlemist_Red",
+        Hybrid: "No",
+      },
+      {
+        image: "https://iili.io/HNOJiVn.jpg",
+        price: "349",
+        name: "Pinkrose_Plant",
+        ratings: "4",
+        image_1: "https://iili.io/HNOdHx9.jpg",
+        image_2: "https://iili.io/HNOdFlj.jpg",
+        image_3: "https://iili.io/HNOdBRV.jpg",
+        about:
+          "The history of pink roses begins around 35 million years ago, when the first trace of a rose was found in fossils. The cultivation of garden roses originated in China about 5,000 years ago. During the Roman Empire, roses were used as medicine, perfume and confetti.",
+        Plant_Name: "Pinkrose_Plant",
+        Type_of_plant: "Flower",
+        Suitable_Location: "Full sun.",
+        Container_Type: "Pot With drainage hole",
+        Height: " 1.5 feet",
+        Brand: "Damask Rose",
+        Planting_Season: "September to October.",
+        table_Plantname: "Pinkrose_Plant",
+        Hybrid: " Curly Pink",
+      },
+      {
+        image: "https://iili.io/HNOKOYb.jpg",
+        price: "359",
+        name: "Dieffenbachia",
+        ratings: "6",
+        image_1: "https://iili.io/HNOqRgn.jpg",
+        image_2: "https://iili.io/HNOqRgn.jpg",
+        image_3: "https://iili.io/HNOoMjj.jpg",
+        about:
+          " Dieffenbachia helps remove toxins from the air, including xylene, to offer you and your family members clean breathing air in your home.",
+        Plant_Name: "Dieffenbachia",
+        Type_of_plant: "Foliage",
+        Suitable_Location: "Indoor",
+        Container_Type: "Pot",
+        Height: " 150 mm",
+        Brand: " go4plants",
+        Planting_Season: " early spring",
+        table_Plantname: "Dieffenbachia",
+        Hybrid: "No",
+      },
+      {
+        image: "https://iili.io/HNOTF9t.webp",
+        price: "140",
+        name: "Miniorange_plant",
+        ratings: "6",
+        image_1: "https://iili.io/HNOTfNn.jpg",
+        image_2: "https://iili.io/HNOTAH7.jpg",
+        image_3: "https://iili.io/HNOTaob.jpg",
+        about:
+          "Low Maintenance. It Requires Less Care,so it can even Be Grown by your Kids. Your Kids Will Be Delighted to Have a Cup of Juice from The Plant they have Planted and Cherished.",
+        Plant_Name: "Miniorange_plant",
+        Type_of_plant: "Fruit",
+        Suitable_Location: " Outdoor",
+        Container_Type: "Plastic Bag",
+        Height: " 300 mm",
+        Brand: "Dwarf Orange tree",
+        Planting_Season: "July to September.",
+        table_Plantname: "Miniorange_plant",
+        Hybrid: "Kamkot lemon",
+      }
+    ])
+  );
+
+}
+
