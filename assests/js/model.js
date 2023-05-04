9076e5cead3b42fa88c496e6da4c96bc554e8081
steffydominic <steffybeena7@gmail.com>
2023-05-04 13:59:01 +0530

    fix:fixed bug on cart page

--- assests/js/model.js
+++ assests/js/model.js
@@ -2,22 +2,19 @@ let login_logo=document.getElementById("log")
 
 if(localStorage.getItem("profile")){
     login_logo.style.display="none"
+    login_title.style.display="none"
     console.log("success")
 }
 else{
     profile_logo.style.display="none"
+    
 
 }
-// const user_name = localStorage.getItem("profile");
-// console.log(user_name);
-// if(localStorage.getItem("profile")){
-// document.getElementById("user_name").innerText= user_name
-  
-// }
 
-let cart_data=document.getElementById("cart")
 
-cart_data.addEventListener("click",e=>{
+let cart_data_view=document.getElementById("cart")
+
+cart_data_view.addEventListener("click",e=>{
   e.preventDefault()
   if(localStorage.getItem("profile")){
   window.location.href="./pages/cart.html"
